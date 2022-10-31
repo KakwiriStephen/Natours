12d0bcf402529aeb44580bb75f11e1323ef1d319
KakwiriStephen <ngottakakwiri@gmail.com>
2022-11-01 00:20:02 +0300

    SignUp email templates

--- utils/email.js
+++ utils/email.js
@@ -28,13 +28,11 @@ module.exports = class Email {
     async send(template, subject) {
         //send actual email
         //Render HTML based on a pug template
-        const html = pug.renderFile(
-            `${__dirname}/../views/emails/${template}.pug`, {
-                firstName: this.firstName,
-                url: this.url,
-                subject,
-            }
-        );
+        const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+            firstName: this.firstName,
+            url: this.url,
+            subject,
+        });
 
         //define email options
         const mailOptions = {

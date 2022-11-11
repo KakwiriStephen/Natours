d2c0dfce4d10c704a77cdc29e3a3021bdae4fe94
KakwiriStephen <ngottakakwiri@gmail.com>
2022-11-12 00:51:10 +0300

    Fixed stripe bug

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,21 +1,31 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(
+    'sk_test_51LzIJFKEDZNBrO6r3jmG2dEmEhvmGMJFwK4FKHV7vep4whbjU9YVwKOBvCtS7NU3ZY3AiS8UCVqmiGshXRL8802W00zoP4VFTR'
+);
 const Tour = require('./../models/tourModels');
 const User = require('./../models/userModels');
 const Booking = require('./../models/bookingModel');
-const catchAsync = require('../utils/catchAsync');
+const catchAsync = require('./../utils/catchAsync');
+// const AppError = require('./../utils/appError')
+const factory = require('./handlerFactory');
 
 exports.getCheckoutSession = catchAsync(async(req, res, next) => {
-    // 1) get currently booked tour
+    // 1) Get the coy booked tour
     const tour = await Tour.findById(req.params.tourId);
-    console.log(tour);
-
-    // 2) create checkout sesssion
+    // 2) Create checkout session
     const session = await stripe.checkout.sessions.create({
         payment_method_types: ['card'],
-        success_url: `${req.protocol}://${req.get('host')}/`,
+        success_url: `${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
         cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
         customer_email: req.user.email,
         client_reference_id: req.params.tourId,
+        // line_items: [{
+        //     name: `${tour.name} Tour`,
+        //     description: tour.summary,
+        //     images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+        //     amount: tour.price * 100,
+        //     currency: 'usd',
+        //     quantity: 1
+        // }]
         line_items: [{
             price_data: {
                 currency: 'usd',
@@ -33,11 +43,70 @@ exports.getCheckoutSession = catchAsync(async(req, res, next) => {
             quantity: 1,
         }, ],
         mode: 'payment',
+
+        //     line_items: [{
+        //     price_data: {
+        //         currency: 'usd',
+        //         product_data: {
+        //             name: 'T-shirt',
+        //         },
+        //         unit_amount: 2000,
+        //     },
+        //     quantity: 1,
+        // }, ],
+        // mode: 'payment',
+        // success_url: 'https://example.com/success',
+        // cancel_url: 'https://example.com/cancel',
+        // line_items.name: `${tour.name} Tour`,
     });
 
-    // 3) Create session as response
+    // 3) Create session as as response
     res.status(200).json({
         status: 'success',
         session,
     });
 });
+
+// exports.createBookingCheckout = catchAsync(async(req, res, next) => {
+//     const { tour, user, price } = req.query;
+
+//     if (!tour && !price && !user) return next();
+
+//     await Booking.create({ tour, user, price });
+
+//     res.redirect(originalUrl.split('?')[0])
+//         // next();
+// })
+
+// const createBookingCheckout = async(session) => {
+//     const tour = session.client_reference_id;
+
+//     const user = (await User.findOne({ email: session.customer_email })).id;
+//     const price = session.amount_total / 100;
+//     await Booking.create({ tour, user, price });
+// };
+
+// exports.webhookCheckout = (req, res, next) => {
+//     const signature = req.headers['stripe-signature'];
+//     let event;
+//     try {
+//         event = stripe.webhooks.constructEvent(
+//             req.body,
+//             signature,
+//             process.env.STRIPE_WEBHOOK_SECRET
+//         );
+//     } catch (err) {
+//         return res.status(400).send(`webhook error: ${err.message}`);
+//     }
+
+//     if (event.type === `checkout.session.completed`)
+//         createBookingCheckout(event.data.object);
+
+//     res.status(200).json({ received: true });
+// };
+
+// exports.createBooking = factory.createOne(Booking);
+// exports.getBooking = factory.getOne(Booking);
+// exports.getAllBookings = factory.getAll(Booking);
+// exports.updateBooking = factory.updateOne(Booking);
+// exports.deleteBooking = factory.deleteOne(Booking);

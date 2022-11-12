1c495d9488e1450803ef2fe73776bec8ec6d96a2
KakwiriStephen <ngottakakwiri@gmail.com>
2022-11-12 12:56:44 +0300

    Stripe Form

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,8 @@
 import '@babel/polyfill';
+import { bookTour } from './stripe';
 import { displayMap } from './mapBox';
 import { login, logout } from './login';
 import { updateSettings } from './updateSettings';
-// import { bookTour } from './stripe';
 
 //DOM ELEMENTS
 const mapBox = document.getElementById('map');
@@ -10,7 +10,7 @@ const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
 const userPasswordForm = document.querySelector('.form-user-password');
-// const bookBtn = document.getElementById('book-tour');
+const bookBtn = document.getElementById('book-tour');
 //VALUES
 
 //DELEGATION
@@ -59,10 +59,10 @@ if (userPasswordForm)
         document.getElementById('password-confirm').value = '';
     });
 
-// if (bookBtn) {
-//     bookBtn.addEventListener('click', (e) => {
-//         e.target.textContent = 'Processing...';
-//         const { tourId } = e.target.dataset;
-//         bookTour(tourId);
-//     });
-// }
+if (bookBtn) {
+    bookBtn.addEventListener('click', (e) => {
+        e.target.textContent = 'Processing...';
+        const { tourId } = e.target.dataset;
+        bookTour(tourId);
+    });
+}

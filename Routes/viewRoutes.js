dfb3e3ca55b6fc44bc199ab6939eb5719bf1c524
KakwiriStephen <ngottakakwiri@gmail.com>
2022-11-12 17:19:05 +0300

    IMplementing Booking

--- Routes/viewRoutes.js
+++ Routes/viewRoutes.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const viewsController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
+const bookingController = require('../controllers/bookingController');
 
 const router = express.Router();
 //    ROUTES
 
-router.get('/', authController.isLoggedIn, viewsController.getOverview);
+router.get(
+    '/',
+    bookingController.createBookingCheckout,
+    authController.isLoggedIn,
+    viewsController.getOverview
+);
 
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 
